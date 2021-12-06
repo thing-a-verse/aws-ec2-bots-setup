cb35c3a488e2802bc1fffc21e4bcc88b817b6065
thing-a-verse <michael@wonkie.net>
2021-12-06 23:54:19 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ function root_pre() {
   # splunk 8.2.2.3
   PACKAGE=splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm
 
-  wget -O $PACKAGE 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm'
+  wget -nv -O $PACKAGE 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm'
   #https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm.md5?_ga=2.171938210.305817112.1638779062-1114357162.1631187882&_gac=1.219988587.1638779791.CjwKCAiAhreNBhAYEiwAFGGKPKEFXeiECB0VRg8cul9UtOYWmTcVqU0IaZVFn_P0ggCJ73UmRJc-yxoCu6gQAvD_BwE&_gl=1*1hhvf2e*_gcl_aw*R0NMLjE2Mzg3NzkzMjguQ2p3S0NBaUFocmVOQmhBWUVpd0FGR0dLUEtFRlhlaUVDQjBWUmc4Y3VsOVV0T1lXbVRjVnFVMElhWlZGbl9QMGdnQ0o3M1VtUkpjLXl4b0N1NmdRQXZEX0J3RQ..
 
   # splunk 8.2.2.2
@@ -65,7 +65,7 @@ function root_post() {
 
 # SVC commands - run as unpriv user, install main application
 function main() {
-  
+
   headline_logger -s "Start ${0} installation as `whoami`"
   SVC=${1}
 
